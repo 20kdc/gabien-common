b9ce2493354c25c95266e5bec4be1bac5e320c62
20kdc <asdd2808@gmail.com>
2017-06-09 01:54:02 +0100

    Change basic UIMapView system to get ready for bugfixing, UIScrollVertLayout now generalized.
    Also this makes the UI layout slightly nicer - no awkward indirection through a menu.

---
+++ src/main/java/gabien/ui/UIScrollLayout.java
@@ -0,0 +1,99 @@
+/*
+ * This is released into the public domain.
+ * No warranty is provided, implied or otherwise.
+ */
+
+package gabien.ui;
+
+import gabien.IGrInDriver;
+import gabien.ui.Rect;
+import gabien.ui.UIElement;
+import gabien.ui.UIPanel;
+import gabien.ui.UIScrollbar;
+
+import java.util.LinkedList;
+
+/**
+ * Basic scrollable layout.
+ * Got moved to gabien.ui on June 9 2017 and redesigned.
+ * Created on 12/29/16.
+ */
+public class UIScrollLayout extends UIPanel {
+    public UIScrollbar scrollbar;
+    public LinkedList<UIElement> panels = new LinkedList<UIElement>();
+    public int scrollLength = 0;
+
+    public UIScrollLayout(boolean vertical) {
+        scrollbar = new UIScrollbar(vertical);
+    }
+
+    public void runLayout() {
+        Rect r = getBounds();
+        allElements.clear();
+        allElements.add(scrollbar);
+        scrollLength = 0;
+
+        if (scrollbar.vertical) {
+            int sbSize = scrollbar.getBounds().width;
+            scrollbar.setBounds(new Rect(r.width - sbSize, 0, sbSize, r.height));
+            for (UIElement p : panels)
+                scrollLength += p.getBounds().height;
+        } else {
+            int sbSize = scrollbar.getBounds().height;
+            scrollbar.setBounds(new Rect(0, r.height - sbSize, r.width, sbSize));
+            for (UIElement p : panels)
+                scrollLength += p.getBounds().width;
+        }
+
+        useScissoring = true;
+        layoutScrollbounds();
+    }
+
+    // Lays out the elements with the current parameters.
+    private void layoutScrollbounds() {
+        allElements.clear();
+        Rect bounds = getBounds();
+        int scrollHeight = scrollLength - (scrollbar.vertical ? bounds.height : bounds.width);
+        int appliedScrollbarSz = scrollbar.vertical ? scrollbar.getBounds().width : scrollbar.getBounds().height;
+        if (scrollHeight <= 0) {
+            scrollHeight = 0;
+            // no need for the scrollbar
+            appliedScrollbarSz = 0;
+        } else {
+            allElements.add(scrollbar);
+        }
+        int rY = (int) (-scrollbar.scrollPoint * scrollHeight);
+        for (UIElement p : panels) {
+            Rect b = p.getBounds();
+            int oRY = rY;
+            if (scrollbar.vertical) {
+                p.setBounds(new Rect(0, rY, bounds.width - appliedScrollbarSz, b.height));
+                rY += b.height;
+                if (oRY <= -b.height)
+                    continue;
+                if (oRY >= bounds.height)
+                    continue;
+            } else {
+                p.setBounds(new Rect(rY, 0, b.width, bounds.height - appliedScrollbarSz));
+                rY += b.width;
+                if (oRY <= -b.width)
+                    continue;
+                if (oRY >= bounds.width)
+                    continue;
+            }
+            allElements.add(p);
+        }
+    }
+
+    @Override
+    public void updateAndRender(int ox, int oy, double DeltaTime, boolean select, IGrInDriver igd) {
+        layoutScrollbounds();
+        super.updateAndRender(ox, oy, DeltaTime, select, igd);
+    }
+
+    @Override
+    public void setBounds(Rect r) {
+        super.setBounds(r);
+        runLayout();
+    }
+}
