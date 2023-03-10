3a11263d5711d6cbf8c2f0ddc740dc4f466abc87
20kdc <asdd2808@gmail.com>
2023-03-10 23:34:56 +0000

    Datum: List forwarder for when ODec isn't in use

---
+++ datum/src/main/java/gabien/datum/DatumExpectListVisitor.java
@@ -0,0 +1,31 @@
+/*
+ * gabien-datum - Quick to implement S-expression format
+ * Written starting in 2023 by contributors (see CREDITS.txt)
+ * To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights to this software to the public domain worldwide. This software is distributed without any warranty.
+ * A copy of the Unlicense should have been supplied as COPYING.txt in this repository. Alternatively, you can find it at <https://unlicense.org/>.
+ */
+package gabien.datum;
+
+/**
+ * Expects a list (for forwarding to DatumKVDVisitor) and nothing else.
+ * Created 10th March 2023.
+ */
+public class DatumExpectListVisitor extends DatumInvalidVisitor {
+    public final Handler handler;
+
+    public DatumExpectListVisitor(Handler handler) {
+        this.handler = handler;
+    }
+
+    @Override
+    public DatumVisitor visitList() {
+        return handler.handle();
+    }
+
+    public interface Handler {
+        /**
+         * Creates a handler
+         */
+        DatumVisitor handle();
+    }
+}
