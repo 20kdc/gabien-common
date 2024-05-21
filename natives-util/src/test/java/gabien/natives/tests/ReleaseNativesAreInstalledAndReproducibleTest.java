d6eb8e0ab0972fd4fc2dc30e68d074b088cc6ff0
20kdc <asdd2808@gmail.com>
2024-05-21 11:35:18 +0100

    kindling-moth release
    hey it fit the theme

--- natives-util/src/test/java/gabien/natives/tests/ReleaseNativesAreInstalledAndReproducibleTest.java
+++ natives-util/src/test/java/gabien/natives/tests/ReleaseNativesAreInstalledAndReproducibleTest.java
@@ -36,7 +36,7 @@ public class ReleaseNativesAreInstalledAndReproducibleTest {
         load();
         String ver = Loader.getNativesVersion();
         // This line is changed after each release.
-        String vrl = "wary-bluebird";
+        String vrl = "kindling-moth";
         // THIS IS THE ONLY PLACE WHERE THIS ENVIRONMENT VARIABLE MAY BE USED.
         // It acts as a - disablable - safety guard with no other side effects.
         @Nullable String dev = System.getenv("GABIEN_NATIVES_DEV");
